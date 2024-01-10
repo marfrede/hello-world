7f61ec2626f9dd6abc9be657bf7cbad05a7edfa8
Marvin Frede <marvinfrede@gmx.de>
2024-01-10 22:12:51 +0100

    optional routing parameters

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const greetings = {
+		en: 'hello',
+		de: 'hallo',
+		es: 'hóla'
+	};
+
+	const lang: string = $page.params.lang ?? 'en';
+	const greeting: string | undefined = greetings[lang as 'en' | 'de' | 'es'];
+</script>
+
+<h1>{greeting}</h1>
