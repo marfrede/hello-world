e8d67a833631e035d8b4269bf75e33e4936eb35d
Marvin Frede <marvinfrede@gmx.de>
2024-01-10 22:02:33 +0100

    read url

--- src/routes/docs/[...slug]/+page.svelte
+++ src/routes/docs/[...slug]/+page.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	const segmentsStr: string = $page.params.slug;
+	const segments = segmentsStr.split('/');
+</script>
+
 <h1>Docs Homepage</h1>
 
 <span>This page catches all routes like</span>
@@ -5,3 +11,9 @@
 	<li>/docs/abc1/kjsadnc</li>
 	<li>/docs/abc2/def3/ghi</li>
 </ul>
+
+{#if segments.length === 1}
+	Viewing Feature {segments[0]}
+{:else}
+	Viewing Concept {segments[1]}
+{/if}
