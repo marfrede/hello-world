c636a1c797cde290317c10568a239928483f157d
Marvin Frede <marvinfrede@gmx.de>
2024-01-11 22:38:35 +0100

    using goto to nav programmatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	function goToBlog() {
+		goto('/blog');
+	}
+</script>
+
 <h1>Hello World</h1>
 
 <a href="/blog">To the blog</a>
+<div>
+	<button on:click={goToBlog}>To The blog per button!</button>
+</div>
