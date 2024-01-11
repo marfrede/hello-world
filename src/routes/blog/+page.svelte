f4a852b965abff3ecb23fc7a4957532fbd4600a9
Marvin Frede <marvinfrede@gmx.de>
2024-01-11 22:36:04 +0100

    list some example products

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+	export let blogIds = [1, 2, 3, 4];
+</script>
+
 <h1>My blog</h1>
+
+<a href="..">Go Back</a><br /><br />
+
+{#each blogIds as blogId}
+	<div><a href="/blog/{blogId}">Blog {blogId}</a></div>
+{/each}
