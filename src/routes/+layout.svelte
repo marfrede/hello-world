33eb9f2dc68bdba6ca9af2330fb8d152ce1b1fbc
Marvin Frede <marvinfrede@gmx.de>
2024-01-12 19:50:50 +0100

    create +layout to display header and footer everywhere

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<header><span>Svelte Tutorial</span></header>
+<slot />
+<footer><span>© marvinfrede</span></footer>
+
+<style lang="scss">
+	header,
+	footer {
+		position: relative;
+		width: 100%;
+		height: 32px;
+		background-color: black;
+
+		@keyframes moveLeftAndRight {
+			0%,
+			100% {
+				transform: translateX(-50%);
+			}
+			50% {
+				transform: translateX(calc(-50% + 32px));
+			}
+		}
+
+		span {
+			color: aliceblue;
+			position: absolute;
+			top: 25%;
+			left: 50%;
+			font-size: 16px;
+
+			font-family: 'Courier New', Courier, monospace;
+			animation: moveLeftAndRight 2s ease infinite;
+		}
+	}
+
+	footer {
+		position: fixed;
+		bottom: 0;
+	}
+</style>
