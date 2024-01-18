ecf4b9780fd098a67a48cf12781c0bbdf0bb7572
Marvin Frede <marvinfrede@gmx.de>
2024-01-18 23:51:49 +0100

    API POST add to comments

--- src/routes/api/comments/+server.ts
+++ src/routes/api/comments/+server.ts
@@ -1,11 +1,18 @@
-import { comments } from '$lib/comments';
+import { comments, type Comment } from '$lib/comments';
 import { json } from '@sveltejs/kit';
+import type { RequestEvent } from '../$types';
 
 export function GET(): Response {
 	return json(comments);
 	// same as: return new Response(JSON.stringify(comments), {headers: {"Content-Type":"application/json"}});
 }
 
-// export function POST(request: Reuq) {
-//     console.log(request);
-// }
+export async function POST({ request }: RequestEvent) {
+	const body = (await request.json()) as { text: string };
+	const comment: Comment = {
+		id: comments.length + 1,
+		text: body.text
+	};
+	comments.push(comment);
+	return json(comments, { status: 201 });
+}
