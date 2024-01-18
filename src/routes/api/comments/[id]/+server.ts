3b686c899b11210903481adbc7dc6352e0040e5c
Marvin Frede <marvinfrede@gmx.de>
2024-01-19 00:58:08 +0100

    API PATCH comments/:id

--- src/routes/api/comments/[id]/+server.ts
+++ src/routes/api/comments/[id]/+server.ts
@@ -6,3 +6,11 @@ export function GET(requestEvent: RequestEvent): Response {
 	const { id } = requestEvent.params as { id: string };
 	return json(comments.find((c) => c.id === +id));
 }
+
+export async function PATCH(requestEvent: RequestEvent): Promise<Response> {
+	const { id } = requestEvent.params as { id: string };
+	const { text } = (await requestEvent.request.json()) as { text: string };
+	const commentIndex = comments.findIndex((c) => c.id === +id);
+	comments[commentIndex].text = text;
+	return json(comments[commentIndex]);
+}
